5c1d6b0f325a1f7cbcc6968861681da0c7222707
QWERKael <d9311@126.com>
2021-07-17 21:45:46 +0800

    使用BufWriter/BufReader优化了代码

--- src/msg/msg.rs
+++ src/msg/msg.rs
@@ -1,21 +1,24 @@
 use core::result::Result;
-use std::io::{Error, Read, Write};
+use std::io::{Error, Read, Write, BufWriter, BufReader};
 use std::net::{TcpListener, TcpStream};
 
 pub fn serv(address: String) -> Result<(), Error> {
     let listener = TcpListener::bind(address)?;
     for stream in listener.incoming() {
-        let mut stream = stream?;
-        let buf = &mut "".to_string();
-        let size = stream.read_to_string(buf)?;
-        println!("获取到 {} 的字符串：{}", size, buf);
+        let stream = stream?;
+        let mut reader = BufReader::new(stream);
+        let buf = &mut [0;4096];
+        let size = reader.read(buf)?;
+        let s = String::from_utf8_lossy(buf);
+        println!("获取到 {} 的字符串：{}", size, s);
     }
     Ok(())
 }
 
 pub fn send(address: String, buf: String) -> Result<(), Error> {
-    let mut stream = TcpStream::connect(address)?;
-    let size = stream.write(String::from(buf.clone()).as_bytes())?;
+    let stream = TcpStream::connect(address)?;
+    let mut writer = BufWriter::new(stream);
+    let size = writer.write(buf.as_bytes())?;
     println!("发送 {} 的字符：{}", size, buf);
     Ok(())
 }
