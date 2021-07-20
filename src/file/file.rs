bb6e0f4865f6102d400e220781b5a1bd666f1629
chenyixuan <chenyixuan@baidu.com>
2021-07-20 18:36:05 +0800

    修复了文件传输最大只能4k的bug

--- src/file/file.rs
+++ src/file/file.rs
@@ -5,22 +5,33 @@ use std::fs::File;
 use std::str;
 use std::convert::TryInto;
 
+const FLOW_SIZE: usize = 4096;
+
 pub fn serv(address: String) -> Result<(), Error> {
     let listener = TcpListener::bind(address)?;
     for stream in listener.incoming() {
         let stream = stream?;
         let mut reader = BufReader::new(stream);
-        let buf = &mut [0;4096];
+        // let buf = &mut [0;25*1024*1024];
+        let buf = &mut [0;FLOW_SIZE];
         let read_size = reader.read(buf)?;
-        println!("获取到buf");
+        // println!("获取到TCP流量 {} bytes",read_size);
         let filename_bytes_len = u64::from_be_bytes(buf[..8].try_into().unwrap()) as usize;
         println!("获取到文件名长度：{}", filename_bytes_len);
         let filename = &buf[8..filename_bytes_len+8];
         let filename = str::from_utf8(&filename).unwrap_or("UnknownName");
         println!("获取到文件名：{}", filename);
         let mut file = File::create(filename)?;
-        let buf_size = file.write(&buf[filename_bytes_len + 8..read_size])?;
-        println!("写入 {} bytes", buf_size);
+        let _buf_size = file.write(&buf[filename_bytes_len + 8..read_size])?;
+        // println!("写入 {} bytes", buf_size);
+        loop{
+            let buf = &mut [0;FLOW_SIZE];
+            let read_size = reader.read(buf)?;
+            // println!("获取到TCP流量 {} bytes",read_size);
+            if read_size < 1 { break; }
+            let _buf_size = file.write(&buf[..read_size])?;
+            // println!("写入 {} bytes", buf_size);
+        }
         break;
     }
     println!("传输结束！");
@@ -28,6 +39,10 @@ pub fn serv(address: String) -> Result<(), Error> {
 }
 
 pub fn send(address: String, path: String) -> Result<(), Error> {
+    if path == "" {
+        println!("发送的文件地址错误！");
+        return Ok(())
+    }
     println!("开始发送");
     let stream = TcpStream::connect(address)?;
     // println!("准备writer");
@@ -43,14 +58,14 @@ pub fn send(address: String, path: String) -> Result<(), Error> {
     let _ = writer.write(&filename_bytes_len.to_be_bytes());
     let _ = writer.write(filename_bytes);
     println!("发送文件名：{}", filename);
-    let mut buf = [0u8; 1024];
+    let mut buf = [0u8; FLOW_SIZE];
     loop {
         match file.read(&mut buf) {
             Ok(read_size) => {
-                println!("读取 {} bytes", read_size);
+                // println!("读取 {} bytes", read_size);
                 if read_size == 0 { break; }
-                let send_size = writer.write(&buf[0..read_size])?;
-                println!("发送 {} bytes", send_size);
+                let _send_size = writer.write(&buf[0..read_size])?;
+                // println!("发送 {} bytes", send_size);
             }
             Err(e) => {
                 println!("发生了一个错误：{}", e);
